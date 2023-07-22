36a0ccb460fa9679fb387c9a7e22bcecdb17320e
Crowdbotics <team@crowdbotics.com>
2023-07-22 21:48:19 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "quantum-battle-budd-42357",
+  url: "https://quantum-battle-budd-42357.botics.co",
+  api: "https://quantum-battle-budd-42357.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
